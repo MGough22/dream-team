d7c471a335ce515e1d9dfde0deb3dfbdfb5bda85
Tom Glencross <tomglencross@Toms-MacBook-Pro-2.local>
2025-01-14 17:03:34 +0000

    added page links, some basic page mocks, some basic components

--- front-end/src/app-components/NavBar1.jsx
+++ front-end/src/app-components/NavBar1.jsx
@@ -1,13 +1,17 @@
 import { Box, Button, Flex, Heading, HStack, Spacer, Text } from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function NavBar1() {
   return (
     <Flex as="nav" p="1vw" alignItems="center" gap="1vw">
-    <Heading as="h1">DreamApp</Heading>
+    <Box as={Link} to="/">
+    <Heading color="black" as="h1" >DreamApp</Heading>
+    </Box>
     <Spacer/>
-<HStack>
-<Box bg="gray.600" p="1vh" color="white">Symbol Guide</Box>
+<HStack spacing="1vw" flexWrap="wrap">
+<Box as={Link} to="/:user/dreamjournal" bg="gray.500" p="1vh" color="white">User Dream Journal</Box>
+<Box as={Link} to="/symbolguide" bg="gray.600" p="1vh" color="white">Symbol Guide</Box>
 <Text bg="gray.700" p="1vh" color="white">Logged in user: Welcome, Dreamer69@dreamz.biz
 </Text>
 <Button bg="gray.800" p="1vh" color="white"> Login/Logout/Settings</Button>
---
+++ front-end/src/app-components/SymbolGuide.jsx
@@ -0,0 +1,8 @@
+import { SimpleGrid } from '@chakra-ui/react'
+import React from 'react'
+
+export default function SymbolGuide() {
+  return (
+   <div>SymbolGuide here</div>
+  )
+}
