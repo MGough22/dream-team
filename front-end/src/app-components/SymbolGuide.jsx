66adad6022b430adfcf46ebfc5f9a96b6c9e9a09
Tom Glencross <tomglencross@Toms-MBP-2.northcoders.com>
2025-01-15 09:38:05 +0000

    continued to add image assets

--- front-end/src/app-components/SymbolGuide.jsx
+++ front-end/src/app-components/SymbolGuide.jsx
@@ -1,8 +1,13 @@
-import { SimpleGrid } from '@chakra-ui/react'
+import { Heading, SimpleGrid } from '@chakra-ui/react'
 import React from 'react'
+import { Image } from '@chakra-ui/react'
+import symbol1 from '../assets/symbol1.png'
 
 export default function SymbolGuide() {
   return (
-   <div>SymbolGuide here</div>
+    <>
+    <Heading>SymbolGuide here</Heading>
+    <Image src={symbol1} alt="stand in image"/>
+    </>
   )
 }
